60e2b10c429d218faf06dec700db5bc10b0b0949
SudzBud <SudzBud@gmail.com>
2015-07-30 16:21:27 -0400

    Work-Around for no local RSAT on WIndows 10

---
+++ NEW-Start-RemoteExchangePowershellConsole.ps1
@@ -0,0 +1,24 @@
+﻿
+if ($(get-host).name -eq "PowerShellPlus Host"){$psplus.Tabs.SetSelectedTab("Console")}
+$exPol = $(Get-ExecutionPolicy).ToString()
+if ($exPol -ne "RemoteSigned"){
+Write-Host -ForegroundColor black -BackgroundColor red "**WARNING Setting ExecutionPolicy to RemoteSigned**"
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force 
+}
+
+$ExServer = Read-Host "Enter Exchange ServerName"
+if (Test-Path "C:\DevelopmentWorkspace\Network Group - Development-Branch\PowerShell Scripts\KSA _Email\AdminCred.clixml"){
+$AdminCred = Import-Clixml "C:\DevelopmentWorkspace\Network Group - Development-Branch\PowerShell Scripts\KSA _Email\AdminCred.clixml"}
+else{$AdminCred = Get-Credential
+}
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionURI http://$ExServer/PowerShell/ -Authentication Kerberos -Credential $AdminCred
+Import-PSSession $Session
+
+$Session2 = New-PSSession -ComputerName ksa2008dc -Authentication Kerberos -Credential $AdminCred
+Invoke-Command -ScriptBlock {Import-Module ActiveDirectory} -Session $Session2
+Import-PSSession $Session2 -Module ActiveDirectory
+
+if ($exPol -ne "RemoteSigned"){
+Write-Host -BackgroundColor yellow -ForegroundColor Black "**Resetting ExecutionPolicy to $exPol**"
+Set-ExecutionPolicy $exPol -Scope process -force
+}
